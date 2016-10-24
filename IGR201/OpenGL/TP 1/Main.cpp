fd6eca8492cc95dc049b9c8bf992be99415f0ec8
Aloïs Pourchot <alois.pourchot@gmail.com>
2016-10-24 15:12:06 +0200

    ajout tp opengl

--- IGR201/OpenGL/TP 1/Main.cpp
+++ IGR201/OpenGL/TP 1/Main.cpp
@@ -10,14 +10,19 @@
 #include <vector>
 #include <string>
 #include <cmath>
-#include <GL/glut.h>
+#include <math.h>
+#include <glut.h>
 #include <vector>
+
+#ifndef M_PI
+	#define M_PI 3.1415
+#endif 
 
 using namespace std;
 
 // App parameters
-static const unsigned int DEFAULT_SCREENWIDTH = 1024;
-static const unsigned int DEFAULT_SCREENHEIGHT = 768;
+static const unsigned int DEFAULT_SCREENWIDTH = 800;
+static const unsigned int DEFAULT_SCREENHEIGHT = 600;
 static string appTitle ("Informatique Graphique & Realite Virtuelle - Travaux Pratiques - Introduction a OpenGL");
 static GLint window;
 static unsigned int screenWidth;
@@ -36,9 +41,9 @@ static float camTargetY;
 static float camTargetZ;
 
 // Sphere parameters
-const int n1=20;
-const int n2=20;
-static float spherePositionArray[3*n1*n2];
+const int n1=30;
+const int n2=30;
+static float spherePositionArray[3*(n1-1)*n2+6];
 static unsigned int sphereIndexArray[6*n1*n2];
 
 void polar2Cartesian (float phi, float theta, float r, float & x, float & y, float & z) {
@@ -47,15 +52,77 @@ void polar2Cartesian (float phi, float theta, float r, float & x, float & y, flo
 	z = r * cos (theta);
   }
 
-void glSphere(float xCoord, float yCoord, float zCoord, float radius){
+void glSphereV2(float xCoord, float yCoord, float zCoord, float radius){
 
 	glMatrixMode (GL_MODELVIEW); // Indique que l’on va désormais altérer la matrice modèle-vue
 	glPushMatrix (); // pousse la matrice courante sur un pile
 	glTranslatef (xCoord, yCoord, zCoord); // applique une translation à la matrice
-	//glScale(radius,radius,radius);
+	glScalef(radius,radius,radius);
 
-	// Put your drawing code (glBegin, glVertex, glCallList, glDrawArray, etc) here
 	glDrawElements(GL_TRIANGLES, 6*n1*n2, GL_UNSIGNED_INT, (GLvoid*) sphereIndexArray);
+
+	glPopMatrix (); // replace la matrice modèle vue courante original
+}
+
+void glSphereV1(float xCoord, float yCoord, float zCoord, float radius)
+{
+
+	glMatrixMode (GL_MODELVIEW); // Indique que l’on va désormais altérer la matrice modèle-vue
+	glPushMatrix (); // pousse la matrice courante sur un pile
+	glTranslatef (xCoord, yCoord, zCoord); // applique une translation à la matrice
+	glScalef(radius,radius,radius);
+
+	float theta=0;
+	float phi=0;
+	float x=0;
+	float y=0;
+	float z=0;
+
+	glBegin(GL_TRIANGLES);
+	for(int k1=0; k1<n1; k1++)
+	{
+
+		for(int k2=0; k2<n2; k2++)
+		{
+
+			theta=k1*M_PI/n1;
+			phi=k2*2*M_PI/n2;
+			polar2Cartesian(phi,theta,1.0,x,y,z);
+			glColor3f(x,y,z);
+			glVertex3f(x,y,z);
+
+			theta=(k1+1)*M_PI/n1;
+			phi=k2*2*M_PI/n2;
+			polar2Cartesian(phi,theta,1.0,x,y,z);
+			glColor3f(x,y,z);
+			glVertex3f(x,y,z);
+
+			theta=(k1+1)*M_PI/n1;
+			phi=(k2+1)*2*M_PI/n2;
+			polar2Cartesian(phi,theta,1.0,x,y,z);
+			glColor3f(x,y,z);
+			glVertex3f(x,y,z);
+
+			theta=k1*M_PI/n1;
+			phi=k2*2*M_PI/n2;
+			polar2Cartesian(phi,theta,1.0,x,y,z);
+			glColor3f(x,y,z);
+			glVertex3f(x,y,z);
+
+			theta=(k1+1)*M_PI/n1;
+			phi=(k2+1)*2*M_PI/n2;
+			polar2Cartesian(phi,theta,1.0,x,y,z);
+			glColor3f(x,y,z);
+			glVertex3f(x,y,z);
+
+			theta=k1*M_PI/n1;
+			phi=(k2+1)*2*M_PI/n2;
+			polar2Cartesian(phi,theta,1.0,x,y,z);
+			glColor3f(x,y,z);
+			glVertex3f(x,y,z);
+		}
+	}
+	glEnd();
 
 	glPopMatrix (); // replace la matrice modèle vue courante original
 }
@@ -77,20 +144,20 @@ void printUsage () {
 
 void init () {
 	// OpenGL initialization
-  glCullFace (GL_BACK);     // Specifies the faces to cull (here the ones pointing away from the camera)
-  glEnable (GL_CULL_FACE); // Enables face culling (based on the orientation defined by the CW/CCW enumeration).
-  glDepthFunc (GL_LESS); // Specify the depth test for the z-buffer
-  glEnable (GL_DEPTH_TEST); // Enable the z-buffer in the rasterization
-	glLineWidth (2.0); // Set the width of edges in GL_LINE polygon mode
-  glClearColor (0.0f, 0.0f, 0.0f, 1.0f); // Background color
+  	glCullFace (GL_BACK);     // Specifies the faces to cull (here the ones pointing away from the camera)
+ 	glEnable (GL_CULL_FACE); // Enables face culling (based on the orientation defined by the CW/CCW enumeration).
+  	glDepthFunc (GL_LESS); // Specify the depth test for the z-buffer
+ 	glEnable (GL_DEPTH_TEST); // Enable the z-buffer in the rasterization
+  	glLineWidth (2.0); // Set the width of edges in GL_LINE polygon mode
+  	glClearColor (0.0f, 0.0f, 0.0f, 1.0f); // Background color
 
 	// Camera initialization
 	fovAngle = 45.f;
 	nearPlane = 0.01;
 	farPlane = 10.0;
-	camPhi = 0; //M_PI/2.0;
-	camTheta = M_PI/2.0;
-	camDist2Target = 10;
+	camPhi = M_PI/2.0;
+	camTheta = M_PI/4.0;
+	camDist2Target = 5.0;
 	camTargetX = 0.0;
 	camTargetY = 0.0;
 	camTargetZ = 0.0;
@@ -102,7 +169,20 @@ void init () {
 	float y=0;
 	float z=0;
 
-	for(int k1=0; k1<n1; k1++)
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 3*sizeof(float), (GLvoid*) spherePositionArray);
+
+	polar2Cartesian(0,0,1.0,x,y,z);
+	spherePositionArray[0]=x;
+	spherePositionArray[1]=y;
+	spherePositionArray[2]=z;
+
+	polar2Cartesian(M_PI,0,1.0,x,y,z);
+	spherePositionArray[3*n2*(n1-1)+3]=x;
+	spherePositionArray[3*n2*(n1-1)+4]=y;
+	spherePositionArray[3*n2*(n1-1)+5]=z;
+
+	for(int k1=1; k1<n1; k1++)
 	{
 
 		for(int k2=0; k2<n2; k2++)
@@ -111,22 +191,43 @@ void init () {
 			theta=k1*M_PI/n1;
 			phi=k2*2*M_PI/n2;
 			polar2Cartesian(phi,theta,1.0,x,y,z);
-
-			spherePositionArray[3*n2*k1+3*k2]=x;
-			spherePositionArray[3*n2*k1+3*k2+1]=y;
-			spherePositionArray[3*n2*k1+3*k2+2]=z;
-
-
-			sphereIndexArray[6*n2*k1+6*k2]=n2*k1+k2;
-			sphereIndexArray[6*n2+k1+6*k2+1]=n2*(k1+1)+k2;
-			sphereIndexArray[6*n2+k1+6*k2+2]=n2*(k1+1)+(k2+1);
-			sphereIndexArray[6*n2+k1+6*k2+3]=n2*k1+k2;
-			sphereIndexArray[6*n2+k1+6*k2+4]=n2*(k1+1)+(k2+1);
-			sphereIndexArray[6*n2+k1+6*k2+5]=n2*k1+(k2+1);
+			spherePositionArray[3+3*n2*(k1-1)+3*k2]=x;
+			spherePositionArray[3+3*n2*(k1-1)+3*k2+1]=y;
+			spherePositionArray[3+3*n2*(k1-1)+3*k2+2]=z;
 		}
 	}
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 3*sizeof(float), (GLvoid*) spherePositionArray);
+
+	for(int k2=0; k2<n2; k2++)
+	{
+
+		sphereIndexArray[3*k2]=0;
+		sphereIndexArray[3*k2+1]=(k2+1+n2)%n2;
+		sphereIndexArray[3*k2+2]=(k2+2+n2)%n2;
+	}
+
+	for(int k2=0; k2<n2; k2++)
+	{
+
+		sphereIndexArray[3*n2+6*n2*(n1-1)+3*k2]=1+n2*(n1-1)+k2;
+		sphereIndexArray[3*n2+6*n2*(n1-1)+3*k2+1]=n2*(n1-1)+1;
+		sphereIndexArray[3*n2+6*n2*(n1-1)+3*k2+2]=1+n2*(n1-1)+(k2+1+n2)%n2;
+	}
+
+	for(int k1=1; k1<n1; k1++)
+	{
+
+		for(int k2=0; k2<n2; k2++)
+		{
+
+			sphereIndexArray[3*n2+6*n2*(k1-1)+3*k2]=1+n2*(k1-1)+k2;
+			sphereIndexArray[3*n2+6*n2*(k1-1)+3*k2+1]=1+n2*k1+k2;
+			sphereIndexArray[3*n2+6*n2*(k1-1)+3*k2+2]=1+n2*k1+(k2+1+n2)%n2;
+
+			sphereIndexArray[3*n2+6*n2*(k1-1)+3*k2+3]=1+n2*(k1-1)+k2;
+			sphereIndexArray[3*n2+6*n2*(k1-1)+3*k2+4]=1+n2*(k1-1)+(k2+1+n2)%n2;
+			sphereIndexArray[3*n2+6*n2*(k1-1)+3*k2+5]=1+n2*k1+(k2+1+n2)%n2;
+		}
+	}
 
 }
 
@@ -145,7 +246,7 @@ void setupCamera () {
 }
 
 void reshape (int w, int h) {
-  screenWidth = w;
+    screenWidth = w;
 	screenHeight = h;
 	aspectRatio = static_cast<float>(w)/static_cast<float>(h);
 	glViewport (0, 0, (GLint)w, (GLint)h); // Dimension of the drawing region in the window
@@ -156,9 +257,8 @@ void display () {
     setupCamera ();
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Erase the color and z buffers.
 
-		glSphere(0,0,0,1);
-
-
+	glSphereV2(0.0,0.0,0.0,1.5);
+	glEnd();
 
     glFlush (); // Ensures any previous OpenGL call has been executed
     glutSwapBuffers ();  // swap the render buffer and the displayed (screen) one
